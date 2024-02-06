62f0b5df68ee621109d8a06fa93f298de3e1940a
okuye <olakuye@gmail.com>
2024-02-06 23:09:39 +0000

    [OK] - Updated on a mac

---
+++ build/CMakeFiles/TradingKLX.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/olakunlekuye/Documents/GitHub/TradingKLX/AlphaVantageAPI.cpp" "CMakeFiles/TradingKLX.dir/AlphaVantageAPI.cpp.o" "gcc" "CMakeFiles/TradingKLX.dir/AlphaVantageAPI.cpp.o.d"
+  "/Users/olakunlekuye/Documents/GitHub/TradingKLX/JSONParser.cpp" "CMakeFiles/TradingKLX.dir/JSONParser.cpp.o" "gcc" "CMakeFiles/TradingKLX.dir/JSONParser.cpp.o.d"
+  "/Users/olakunlekuye/Documents/GitHub/TradingKLX/TradingKLX.cpp" "CMakeFiles/TradingKLX.dir/TradingKLX.cpp.o" "gcc" "CMakeFiles/TradingKLX.dir/TradingKLX.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
