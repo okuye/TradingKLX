9e9dc32f9f26e57e8f6235ad3a2e46c2efa311f6
okuye <olakuye@gmail.com>
2024-02-07 09:34:47 +0000

    Add .gitignore to exclude build dependencies

--- build/_deps/nlohmann_json-build/cmake_install.cmake
+++ build/_deps/nlohmann_json-build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/User/source/repos/TradingKLX
+# Install script for directory: C:/Users/User/source/repos/TradingKLX/build/_deps/nlohmann_json-src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -37,19 +37,3 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "C:/cygwin64/bin/objdump.exe")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for each subdirectory.
-  include("C:/Users/User/source/repos/TradingKLX/build/_deps/nlohmann_json-build/cmake_install.cmake")
-
-endif()
-
-if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/User/source/repos/TradingKLX/build/${CMAKE_INSTALL_MANIFEST}"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- build/_deps/nlohmann_json-subbuild/cmake_install.cmake
+++ build/_deps/nlohmann_json-subbuild/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/User/source/repos/TradingKLX
+# Install script for directory: C:/Users/User/source/repos/TradingKLX/build/_deps/nlohmann_json-subbuild
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/TradingKLX")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/nlohmann_json-populate")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -32,17 +32,6 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-# Set default install directory permissions.
-if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/cygwin64/bin/objdump.exe")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for each subdirectory.
-  include("C:/Users/User/source/repos/TradingKLX/build/_deps/nlohmann_json-build/cmake_install.cmake")
-
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -51,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/User/source/repos/TradingKLX/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/User/source/repos/TradingKLX/build/_deps/nlohmann_json-subbuild/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
